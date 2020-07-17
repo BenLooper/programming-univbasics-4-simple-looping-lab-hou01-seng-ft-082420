83d9b2c20851415e66671e3d86371e25a2e6585e
Benjamin Arthur Looper <benlooper08@gmail.com>
2020-07-17 23:22:02 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,34 @@
 # Write your methods here
+
+def loop_message_five_times(string)
+  counter = 0 
+  while counter < 5 
+    puts string
+    counter += 1 
+  end 
+end
+
+def loop_message_n_times(string,integer)
+  counter = 0 
+  while counter < integer 
+    puts string
+    counter += 1 
+  end
+end
+
+def output_array(array)
+  for i in array 
+    puts i 
+  end
+end
+    
+def return_string_array(array)
+  new_array = []
+  for i in array
+    new_array.append(i.to_s)
+  end
+  new_array
+end
+
+  
+  
